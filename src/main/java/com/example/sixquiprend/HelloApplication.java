11bbc71eb1ff180f065be7f7febc0feb1c62e837
queenlolo <lorie.hu@hotmail.com>
2023-05-28 19:06:11 +0200

    interface

--- src/main/java/com/example/sixquiprend/HelloApplication.java
+++ src/main/java/com/example/sixquiprend/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.sixquiprend;
 
+import com.example.sixquiprend.Vue.Interface.Window;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,6 +19,6 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        new Window();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.sixquiprend {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.desktop;
+
+
     opens com.example.sixquiprend to javafx.fxml;
     exports com.example.sixquiprend;
 }
