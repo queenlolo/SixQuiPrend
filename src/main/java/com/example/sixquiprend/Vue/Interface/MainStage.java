f90e368165e70481eb97c0ec187d465e5f43601b
queenlolo <lorie.hu@hotmail.com>
2023-05-31 18:39:15 +0200

    demander nom, message d'info

--- src/main/java/com/example/sixquiprend/Vue/Interface/MainStage.java
+++ src/main/java/com/example/sixquiprend/Vue/Interface/MainStage.java
@@ -1,11 +1,32 @@
 package com.example.sixquiprend.Vue.Interface;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
-public class MainStage extends VBox {
+public class MainStage extends StackPane {
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
 
     public MainStage(){
+        alert.setTitle("Information message");
+        alert.setHeaderText(null);
+        alert.setContentText("Welcome to our 6 qui prend\n\n\n" + "Lorlay, Massil, Lorie");
+        alert.showAndWait();
         this.getChildren().addAll();
+
+        askName();
+    }
+
+    public void askName(){
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Name");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Choose your name:");
+        dialog.showAndWait().ifPresent(userName -> {
+            String name = userName;
+        });
+
     }
 
 }
