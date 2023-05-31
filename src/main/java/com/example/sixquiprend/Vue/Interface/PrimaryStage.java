70a47b99add23e5d8fbf18480e5f45a433f5a6fc
queenlolo <lorie.hu@hotmail.com>
2023-05-31 16:30:30 +0200

    interface ranger

---
+++ src/main/java/com/example/sixquiprend/Vue/Interface/PrimaryStage.java
@@ -0,0 +1,20 @@
+package com.example.sixquiprend.Vue.Interface;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+public class PrimaryStage extends StackPane {
+    Button btnPlay = new Button("START");
+    Image image = new Image("file:src/main/java/com/example/sixquiprend/Vue/Image/menu.jpg");
+    ImageView imageView = new ImageView(image);
+
+    public PrimaryStage(){
+        imageView.setFitWidth(1000);
+        imageView.setFitHeight(700);
+        btnPlay.getStyleClass().add("btnPlay");
+
+        this.getChildren().addAll(imageView, btnPlay);
+    }
+}
